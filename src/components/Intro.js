54ec55059f3f7b0bb67b225f4567f1e4a92bb1b7
Manish Guhe <manishguhe301@gmail.com>
2023-06-20 12:10:43 +0530

    commit

---
+++ src/components/Intro.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import Bride from '../assets/Bride.svg';
+import Sun from '../assets/sun.svg';
+import { TextField } from '@mui/material';
+
+const StyledWrapper = styled.div`
+  height: 450px;
+  background: #173535;
+  position: relative;
+`;
+
+const StyledBrideImage = styled.img`
+  width: 350px;
+  position: absolute;
+  bottom: 0;
+  z-index: 1;
+  right: 174px;
+`;
+
+const StyledSunImage = styled.img`
+  width: 350px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
+const Intro = () => {
+  return (
+    <div>
+      <StyledWrapper>
+        <div style={{ paddingTop: '4em', paddingLeft: '4em' }}>
+          <h1
+            style={{
+              fontWeight: 700,
+              color: ' rgba(255, 255, 255, 0.99)',
+            }}
+          >
+            Plan your love journey with <br></br>us right by your side
+          </h1>
+          <TextField type='text' value='04 January 1998' label='Outlined' />
+          <TextField type='text' value='04 January 1998' label='Outlined' />
+        </div>
+        <StyledBrideImage src={Bride} alt='bride' />
+        <StyledSunImage src={Sun} alt='Sun' />
+      </StyledWrapper>
+    </div>
+  );
+};
+
+export default Intro;
